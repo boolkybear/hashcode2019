e1331e7079034a21a3c9bd6caa6705fadfb6a7a2
Jose Servet Font <jose.servet.font@everis.com>
2019-02-28 19:43:51 +0100

    Initial Commit

---
+++ SlideShow/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SlideShow
+//
+//  Created by Jose Servet Font on 28/02/2019.
+//  Copyright © 2019 Jose Servet Font. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

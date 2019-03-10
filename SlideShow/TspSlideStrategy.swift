31cb1c06cdcc97c671675550bd14faf850fcd98a
Jose Servet Font <jose.servet.font@everis.com>
2019-03-10 01:00:43 +0100

    save distance map

--- SlideShow/TspSlideStrategy.swift
+++ SlideShow/TspSlideStrategy.swift
@@ -43,82 +43,107 @@ extension TspSlideStrategy: SlideStrategy {
         let validSlides = Array(slides.filter { $0.tags.count > 1 })
         let slideCount = validSlides.count
         let slideRange = 0..<slideCount
-        var values: [Int] = Array(repeating: Int.min, count: slideCount * slideCount)
-//        let values: [[Int]] = slideRange.map { j in
-//            let jslide = validSlides[j]
-//            print("Calculating distances \(TspSlideStrategy.name) - \(j)/\(validSlides.count)")
-//            return slideRange.map { i in
-//                if i == j {
-//                    return Int.min
+
+        func calculateDistances() -> [[Int]] {
+            let distanceFileName = "distanceMap.txt"
+//
+//            if let inputString = try? String(contentsOfFile: distanceFileName) {
+//                print("Reading distances \(TspSlideStrategy.name) from file")
+//
+//                let lines = inputString.components(separatedBy: "\n").filter { !$0.isEmpty }
+//                return lines.map {
+//                    $0.components(separatedBy: ",").map { Int($0)! }
 //                }
-//                return TspSlideStrategy.calculateValue(first: jslide, second: validSlides[i])
 //            }
-//        }
+
+            let values: [[Int]] = slideRange.map { j in
+                let jslide = validSlides[j]
+                print("Calculating distances \(TspSlideStrategy.name) - \(j)/\(validSlides.count)")
+                let subrange = (j + 1)..<validSlides.count
+                return subrange.map { i in
+                    return TspSlideStrategy.calculateValue(first: jslide, second: validSlides[i])
+//                    return 1
+                }
+            }
+
+            let valuesString = values.map {
+                $0.map { "\($0)" }.joined(separator: ",")
+            }
+            let singleString = valuesString.joined(separator: "\n")
+            do {
+                try? singleString.write(toFile: distanceFileName, atomically: true, encoding: .utf8)
+            }
+
+            return values
+        }
+
+        let values = calculateDistances()
 
         func getValue(j: Int, i: Int) -> Int {
-            return values[j * slideCount + i]
-        }
-
-        func setValue(j: Int, i: Int, value: Int) {
-            values[j * slideCount + i] = value
-        }
-
-        slideRange.forEach { j in
-            let jslide = validSlides[j]
-            print("Calculating distances \(TspSlideStrategy.name) - \(j)/\(validSlides.count)")
-            let subrange = (j + 1)..<validSlides.count
-            subrange.forEach { i in
-                let value = TspSlideStrategy.calculateValue(first: jslide, second: validSlides[i])
-                setValue(j: j, i: i, value: value)
-                setValue(j: i, i: j, value: value)
-            }
-        }
-
-        var liveNodes: [Int: Node] = [:]
-        slideRange.forEach {
-            liveNodes[$0] = Node(startNode: $0, endNode: $0, value: 0, path: [$0])
+            let mini = min(j, i)
+            let maxi = max(j, i)
+            return values[mini][maxi - mini - 1]
         }
 
         func subgraphValue(from: Node, to: Node) -> Int {
             return from.value + getValue(j: from.endNode, i: to.startNode) + to.value
         }
 
-        while liveNodes.count > 1 {
-            let keys = Array(liveNodes.keys)
-            var bestOrigin = keys.first!
-            var bestEnd = keys.last!
-            let bestOriginNode = liveNodes[bestOrigin]!
-            let bestEndNode = liveNodes[bestEnd]!
-            var bestValue = subgraphValue(from: bestOriginNode, to: bestEndNode)
-
-            liveNodes.forEach { sourceIndex, sourceNode in
-//                print("Finding max \(TspSlideStrategy.name) - \(sourceIndex)/\(keys.count)")
-
-                liveNodes.forEach { endIndex, endNode in
-                    if sourceIndex == endIndex {
-                        return
-                    }
-
-                    let value = subgraphValue(from: sourceNode, to: endNode)
-                    if value > bestValue {
-                        bestOrigin = sourceIndex
-                        bestEnd = endIndex
-                        bestValue = value
-                    }
-                }
-            }
-
-            let sourceNode = liveNodes[bestOrigin]!
-            let endNode = liveNodes[bestEnd]!
-            let newOriginNode = Node(startNode: bestOrigin, endNode: endNode.endNode, value: bestValue, path: sourceNode.path + endNode.path)
-            liveNodes[bestOrigin] = newOriginNode
-            liveNodes[bestEnd] = nil
-
-            print("Live nodes \(TspSlideStrategy.name) \(liveNodes.count)/\(bestValue)")
+        let maxNodes = 1100
+        var groups = slideCount / maxNodes
+        if slideCount % maxNodes != 0 {
+            groups += 1
         }
 
-        let bestSolution = liveNodes.first!.value
+        var maxPath: [Int] = []
 
-        return bestSolution.path.map { validSlides[$0] }
+        (0..<groups).forEach { group in
+            let lowerBound = group * maxNodes
+            let upperBound = lowerBound + maxNodes
+
+            var liveNodes: [Int: Node] = [:]
+            slideRange.filter { lowerBound <= $0 && $0 < upperBound }.forEach {
+                liveNodes[$0] = Node(startNode: $0, endNode: $0, value: 0, path: [$0])
+            }
+
+            while liveNodes.count > 1 {
+                let keys = Array(liveNodes.keys)
+                var bestOrigin = keys.first!
+                var bestEnd = keys.last!
+                let bestOriginNode = liveNodes[bestOrigin]!
+                let bestEndNode = liveNodes[bestEnd]!
+                var bestValue = subgraphValue(from: bestOriginNode, to: bestEndNode)
+
+                liveNodes.forEach { sourceIndex, sourceNode in
+    //                print("Finding max \(TspSlideStrategy.name) - \(sourceIndex)/\(keys.count)")
+
+                    liveNodes.forEach { endIndex, endNode in
+                        if sourceIndex == endIndex {
+                            return
+                        }
+
+                        let value = subgraphValue(from: sourceNode, to: endNode)
+                        if value > bestValue {
+                            bestOrigin = sourceIndex
+                            bestEnd = endIndex
+                            bestValue = value
+                        }
+                    }
+                }
+
+                let sourceNode = liveNodes[bestOrigin]!
+                let endNode = liveNodes[bestEnd]!
+                let newOriginNode = Node(startNode: bestOrigin, endNode: endNode.endNode, value: bestValue, path: sourceNode.path + endNode.path)
+                liveNodes[bestOrigin] = newOriginNode
+                liveNodes[bestEnd] = nil
+
+                print("(\(group)/\(groups)) Live nodes \(TspSlideStrategy.name) \(liveNodes.count)/\(bestValue)")
+            }
+
+            let bestSolution = liveNodes.first!.value
+            maxPath.append(contentsOf: bestSolution.path)
+        }
+
+        return maxPath.map { validSlides[$0] }
     }
 }
